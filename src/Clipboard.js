f5276c7c897cce103d012ad30f63e44eff893eb4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-03-13 14:30:29 +0100

    added files

---
+++ src/Clipboard.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+ 
+import copy from "copy-to-clipboard";
+ 
+import "./App.css";
+ 
+class CopyBoard extends Component {
+ 
+  constructor() {
+    super();
+ 
+    this.state = {
+      textToCopy: "Copy to Clipboard Demo!",
+    };
+ 
+    this.handleInputChange = this.handleInputChange.bind(this);
+ 
+    this.Copytext = this.Copytext.bind(this);
+  }
+ 
+  handleInputChange(e) {
+    this.setState({
+      textToCopy: e.target.value,
+    });
+  }
+ 
+  Copytext() {
+    copy(this.state.textToCopy);
+  }
+ 
+  render() {
+    const { textToCopy, btnText } = this.state;
+ 
+    return (
+      <div className="container">
+        <div class="row" className="hdr">
+          <div class="col-sm-12 btn btn-info">Copy to Clipboard Demo</div>
+        </div>
+ 
+        <div className="txt">
+          <textarea
+            className="form-control"
+            placeholder="Enter Text"
+            onChange={this.handleInputChange}
+          />
+ 
+          <br />
+ 
+          <br />
+ 
+          <button className="btn btn-info" onClick={this.Copytext}>
+            Copy to Clipboard
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+ 
+export default CopyBoard;
